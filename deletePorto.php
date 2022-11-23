cbc390f30908b81a262ff99fd0f1cd36e4f3cfb5
alif ndaru kusuma <alifalip6727@gmail.com>
2022-11-24 04:41:11 +0700

    fixing deletePorto

--- deletePorto.php
+++ deletePorto.php
@@ -1,5 +1,27 @@
 <?php 
 include "config.php";
+$querySelect = "SELECT * FROM porto WHERE porto_id = ".$_GET['porto_id'];
+$ResultSelectStmt = mysqli_query($koneksi,$querySelect);
+$fetchRecords = mysqli_fetch_assoc($ResultSelectStmt);
 
+$fetchImgTitleName = $fetchRecords['foto'];
+// var_dump($fetchImgTitleName);
 
+$createDeletePath = "images/".$fetchImgTitleName;
+
+if(unlink($createDeletePath))
+{
+  $liveSqlQQ = "DELETE FROM porto WHERE porto_id = ".$fetchRecords['porto_id'];
+  $rsDelete = mysqli_query($koneksi, $liveSqlQQ);	
+  
+  if($rsDelete)
+  {
+    header('location:allporto.php?success=true');
+    exit();
+  }
+}
+else
+{
+  $displayErrMessage = "Unable to delete Image";
+}
 ?>
